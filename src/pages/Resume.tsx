623524d1a44af6a32a81198e096998c1a31f64dc
RAKKUNN <njkat0108@gmail.com>
2024-05-27 21:45:45 +0900

    real_final

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,15 @@
 const Resume = () => {
-  return <div>Computer Science & Engineering</div>;
+  return (
+    <div>
+      <h1>Hello, I'm WooJin Im who develops systemic and creative codes</h1>
+
+      <h2>Education : Chungnam National Univerity</h2>
+      <h3>Major - Computer Science & Engineering</h3>
+      <h4>2020 ~ 2026</h4>
+      <h3>Micro - Information security</h3>
+      <h4>2024 ~ 2026</h4>
+    </div>
+  );
 };
 
 export default Resume;
